50c85532e1ccad4d935b7c4b8aee45b8b0bee948
Roger E Critchlow Jr <rec@elf.org>
2011-02-24 00:29:30 -0700

    Trivial changes from sdr-widget incorporated and tested.  Non-trivial to be tested.

--- src/taskEXERCISE.c
+++ src/taskEXERCISE.c
@@ -3,22 +3,9 @@
 
 #include "board.h"
 
-#include "gpio.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "usb_drv.h"
-#include "usb_descriptors.h"
-#include "usb_standard_request.h"
-#include "usb_specific_request.h"
-#include "device_audio_task.h"
 #include "taskEXERCISE.h"
-#include "PCF8574.h"
-#include "taskAK5394A.h"
-
-#if LCD_DISPLAY			// Multi-line LCD display
-#include "taskLCD.h"
-#endif
 
 static void vtaskEXERCISE( void * pcParameters ) {
 	portTickType xLastWakeTime;
@@ -30,8 +17,6 @@ static void vtaskEXERCISE( void * pcParameters ) {
 
     }
 }
-
-
 
 void vStartTaskEXERCISE( unsigned portBASE_TYPE uxPriority )
 {
