12cf8dccdd61533bb82cbfbb3757ecbcec4e8e11
Roger E Critchlow Jr <rec@elf.org>
2011-02-26 15:48:27 -0700

    Start of hpsdr merging.

--- src/hpsdr_image.c
+++ src/hpsdr_image.c
@@ -8,6 +8,55 @@
 
 #include "image.h"
 
+#ifndef FREERTOS_USED
+#if (defined __GNUC__)
+#include "nlao_cpu.h"
+#include "nlao_usart.h"
+#endif
+#else
+#include <stdio.h>
+#endif
+#include "compiler.h"
+#include "board.h"
+#include "print_funcs.h"
+#include "intc.h"
+#include "pm.h"
+#include "gpio.h"
+#ifdef FREERTOS_USED
+#include "FreeRTOS.h"
+#include "task.h"
+#endif
+#include "conf_usb.h"
+#include "usb_task.h"
+#if USB_DEVICE_FEATURE == ENABLED
+#include "device_mouse_hid_task.h"
+#endif
+#if USB_HOST_FEATURE == ENABLED
+//#include "host_keyboard_hid_task.h"
+//#include "host_mouse_hid_task.h"
+#endif
+#include "composite_widget.h"
+#include "taskAK5394A.h"
+#include "hpsdr_taskAK5394A.h"
+//#include "I2C.h"
+
+/*
+ * Task specific headers.
+ */
+
+#include "queue.h"
+#include "taskEXERCISE.h"
+#include "taskMoboCtrl.h"
+#include "taskPowerDisplay.h"
+#include "taskPushButtonMenu.h"
+#include "device_audio_task.h"
+#include "hpsdr_device_audio_task.h"
+#include "wdt.h"
+
+#if LCD_DISPLAY				// Multi-line LCD display
+#include "taskLCD.h"
+#endif
+
 // image launch
 static void x_image_boot(void) {
 }
@@ -16,6 +65,23 @@ static void x_image_init(void) {
 }
 
 static void x_image_task_init(void) {
+  // Initialize USB task
+  usb_task_init();
+
+#if USB_DEVICE_FEATURE == ENABLED
+
+  mutexEP_IN = xSemaphoreCreateMutex(); // for co-ordinating multiple tasks using EP IN
+
+  #if LCD_DISPLAY						// Multi-line LCD display
+  vStartTaskLCD();
+  vStartTaskPowerDisplay();
+  vStartTaskPushButtonMenu();
+  #endif
+  vStartTaskMoboCtrl();
+  vStartTaskEXERCISE( tskIDLE_PRIORITY );
+  hpsdr_AK5394A_task_init();
+  hpsdr_device_audio_task_init(HPSDR_EP_IQ_IN, HPSDR_EP_IQ_OUT, 0);
+#endif
 }
 
 // descriptor accessors
