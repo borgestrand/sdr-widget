5a09e7d86f9d4b25667612f27b1507ae94e4d640
Graeme Jury <gvjury@gmail.com>
2011-08-28 11:52:51 +1200

    Coded option to save window geometry

--- trunk/src/QtRadio/UI.cpp
+++ trunk/src/QtRadio/UI.cpp
@@ -283,7 +283,7 @@ void UI::actionAbout() {
 
 void UI::loadSettings() {
     QSettings settings("G0ORX", "QtRadio");
-    qDebug() << "saveSettings: " << settings.fileName();
+    qDebug() << "loadSettings: " << settings.fileName();
 
     band.loadSettings(&settings);
     xvtr.loadSettings(&settings);
@@ -296,6 +296,14 @@ void UI::loadSettings() {
     if(settings.contains("gain")) gain=subRxGain=settings.value("gain").toInt();
     if(settings.contains("agc")) agc=settings.value("agc").toInt();
     settings.endGroup();
+
+    settings.beginGroup("mainWindow");
+    qDebug() << "######################################################## getGeometryState = " << configure.getGeometryState();
+    if (configure.getGeometryState()) {
+        restoreGeometry(settings.value("geometry").toByteArray());
+    }
+    settings.endGroup();
+
     myVfo->readSettings(&settings);
 }
 
@@ -319,6 +327,11 @@ void UI::saveSettings() {
     settings.setValue("subRxGain",subRxGain);
     settings.setValue("agc",agc);
     settings.endGroup();
+
+    settings.beginGroup("mainWindow");
+    settings.setValue("geometry", saveGeometry());
+    settings.endGroup();
+
     myVfo->writeSettings(&settings);
 }
 
