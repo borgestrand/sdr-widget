316089791062fb24bfead10816e6919983016b68
Andrea Montefusco IW0HDV <andrew@montefusco.com>
2011-09-16 13:53:37 +0200

    PPA release 1.4.2 Minor bug fixed.

--- cleanup.sh
+++ cleanup.sh
@@ -17,29 +17,9 @@ rm -f -r	trunk/src/DttSP/.deps/
 rm -f		trunk/src/DttSP/Makefile
 rm -f		trunk/src/DttSP/Makefile.in
 rm -f -r	trunk/src/QtRadio/.deps/
-rm -f -r	trunk/src/QtRadio/About.moc.cpp
-rm -f -r	trunk/src/QtRadio/Audio.moc.cpp
-rm -f -r	trunk/src/QtRadio/Band.moc.cpp
-rm -f -r	trunk/src/QtRadio/BandStackEntry.moc.cpp
-rm -f -r	trunk/src/QtRadio/Bandscope.moc.cpp
-rm -f -r	trunk/src/QtRadio/Bookmark.moc.cpp
-rm -f -r	trunk/src/QtRadio/BookmarkDialog.moc.cpp
-rm -f -r	trunk/src/QtRadio/Bookmarks.moc.cpp
-rm -f -r	trunk/src/QtRadio/BookmarksDialog.moc.cpp
-rm -f -r	trunk/src/QtRadio/BookmarksEditDialog.moc.cpp
-rm -f -r	trunk/src/QtRadio/Configure.moc.cpp
-rm -f -r	trunk/src/QtRadio/Connection.moc.cpp
-rm -f -r	trunk/src/QtRadio/Filter.moc.cpp
-rm -f -r	trunk/src/QtRadio/Filters.moc.cpp
-rm -f -r	trunk/src/QtRadio/FiltersBase.moc.cpp
-rm -f -r	trunk/src/QtRadio/KeypadDialog.moc.cpp
+rm -f -r	trunk/src/QtRadio/*.moc.cpp
 rm -f -r	trunk/src/QtRadio/Makefile
 rm -f -r	trunk/src/QtRadio/Makefile.in
-rm -f -r	trunk/src/QtRadio/Mode.moc.cpp
-rm -f -r	trunk/src/QtRadio/Spectrum.moc.cpp
-rm -f -r	trunk/src/QtRadio/UI.moc.cpp
-rm -f -r	trunk/src/QtRadio/Waterfall.moc.cpp
-rm -f -r	trunk/src/QtRadio/Xvtr.moc.cpp
 rm -f -r	trunk/src/QtRadio/aclocal.m4
 rm -f -r	trunk/src/QtRadio/autom4te.cache/
 rm -f -r	trunk/src/QtRadio/build-aux/libtool.m4
@@ -57,23 +37,13 @@ rm -f -r	trunk/src/QtRadio/install-sh
 rm -f -r	trunk/src/QtRadio/libtool
 rm -f -r	trunk/src/QtRadio/ltmain.sh
 rm -f -r	trunk/src/QtRadio/missing
-rm -f -r	trunk/src/QtRadio/smeter.moc.cpp
-rm -f -r	trunk/src/QtRadio/ui_About.h
-rm -f -r	trunk/src/QtRadio/ui_Bandscope.h
-rm -f -r	trunk/src/QtRadio/ui_Bookmark.h
-rm -f -r	trunk/src/QtRadio/ui_BookmarksDialog.h
-rm -f -r	trunk/src/QtRadio/ui_BookmarksEditDialog.h
-rm -f -r	trunk/src/QtRadio/ui_Configure.h
-rm -f -r	trunk/src/QtRadio/ui_KeypadDialog.h
-rm -f -r	trunk/src/QtRadio/ui_UI.h
-rm -f -r	trunk/src/QtRadio/ui_vfo.h
-rm -f -r	trunk/src/QtRadio/vfo.moc.cpp
+rm -f -r	trunk/src/QtRadio/ui_*.h
 rm -f -r	trunk/src/dspserver/.deps/
 rm -f -r	trunk/src/dspserver/Makefile
 rm -f -r	trunk/src/dspserver/Makefile.in
 rm -f -r	trunk/src/softrock/.deps/
 rm -f -r	trunk/src/softrock/Makefile
 rm -f -r	trunk/src/softrock/Makefile.in
-rm -f -r	ghpsdr3-1.2.tar.gz
+rm -f -r	ghpsdr3-1.*.tar.gz
 rm -f		trunk/src/QtRadio/GITSTR
 
