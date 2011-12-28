6d2b7668c4bfdc88158fc41c6597fb5983a728fa
Alex Lee <alexlee188@gmail.com>
2011-12-28 18:55:10 +0800

    fixed uac1 100ms mute

--- src/uac1_usb_specific_request.c
+++ src/uac1_usb_specific_request.c
@@ -159,8 +159,6 @@ void uac1_user_set_interface(U8 wIndex, U8 wValue) {
 	} else if (usb_interface_nb == STD_AS_INTERFACE_OUT){
 		usb_alternate_setting_out = wValue;
 		usb_alternate_setting_out_changed = TRUE;
-		usb_alternate_setting_out = Usb_read_endpoint_data(EP_CONTROL, 8);
-/*
 		if (usb_alternate_setting_out != 1) {
 			spk_mute = TRUE;						// mute speaker immediately
 			pdca_disable(PDCA_CHANNEL_SSC_TX);		// stop speaker PDAC
@@ -169,8 +167,6 @@ void uac1_user_set_interface(U8 wIndex, U8 wValue) {
 			spk_mute = FALSE;
 			pdca_enable(PDCA_CHANNEL_SSC_TX);
 			};
-*/
-
 	}
 }
 
