1184493573192848165389b6e2b1ece5b89112d0
Alex Lee <alexlee188@gmail.com>
2012-04-06 12:20:52 +0800

    added #define _LINUX_QUIRK

--- src/uac2_taskAK5394A.c
+++ src/uac2_taskAK5394A.c
@@ -63,7 +63,8 @@
 //_____ M A C R O S ________________________________________________________
 
 //_____ D E F I N I T I O N S ______________________________________________
-
+// uncomment for Linux
+// #define _LINUX_QUIRK
 //_____ D E C L A R A T I O N S ____________________________________________
 
 void uac2_AK5394A_task(void*);
@@ -183,7 +184,7 @@ void uac2_AK5394A_task(void *pvParameters) {
 							0);                 // divided by 2.  Therefore GCLK1 = 6.144Mhz
 				pm_gc_enable(&AVR32_PM, AVR32_PM_GCLK_GCLK1);
 
-#ifdef LINUX_QUIRK
+#ifdef _LINUX_QUIRK
 				FB_rate = (96) << 15;
 #else
 				FB_rate = (96) << 14;
@@ -212,7 +213,7 @@ void uac2_AK5394A_task(void *pvParameters) {
 								  1,                  // diven - enabled
 								  0);                 // divided by 2.  Therefore GCLK1 = 6.144Mhz
 				pm_gc_enable(&AVR32_PM, AVR32_PM_GCLK_GCLK1);
-#ifdef LINUX_QUIRK
+#ifdef _LINUX_QUIRK
 				FB_rate = (88 << 15) + (1<<15)/5;
 #else
 				FB_rate = (88 << 14) + (1<<14)/5;
