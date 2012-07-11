9c69b830e8c0e02f56f73b76ffe4c9df9f375284
Borge Strand-Bergesen <borge.strand@gmail.com>
2012-07-11 23:09:38 +0200

    GPL copyright notice device_mouse_hid_task.c

--- src/device_mouse_hid_task.c
+++ src/device_mouse_hid_task.c
@@ -46,6 +46,26 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
  *
+ * Additions and Modifications to ATMEL AVR32-SoftwareFramework-AT32UC3 are:
+ *
+ * Copyright (C) Borge Strand-Bergesen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Modified by Alex Lee and sdr-widget team since Feb 2010.  Copyright General Purpose Licence v2.
+ * Please refer to http://code.google.com/p/sdr-widget/
  */
 
 //_____  I N C L U D E S ___________________________________________________
