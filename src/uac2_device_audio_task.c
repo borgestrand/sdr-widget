20579039b99bd1970a3c2ffabdb57d497f3fda31
Alex Lee <alexlee188@gmail.com>
2012-08-10 17:30:42 +0800

    Tweaks to rate feedback for fit Linux and Windows

--- src/uac2_device_audio_task.c
+++ src/uac2_device_audio_task.c
@@ -322,13 +322,17 @@ void uac2_device_audio_task(void *pvParameters)
 
 							if (((current_freq.frequency == 88200) && (FB_rate > ((88 << 14) + (7 << 14)/10))) ||
 								((current_freq.frequency == 96000) && (FB_rate > ((96 << 14) + (5 << 14)/10))))
-								FB_rate -= FB_RATE_DELTA * 256;
+								FB_rate -= FB_RATE_DELTA * 128;
 
 							FB_rate -= FB_RATE_DELTA;
 							old_gap = gap;
 						} else
 							if ( (gap > (SPK_BUFFER_SIZE + (SPK_BUFFER_SIZE/2))) && (gap > old_gap)) {
 							LED_Toggle(LED1);
+
+							if ((current_freq.frequency == 88200) && (FB_rate > ((92 << 14) + (2 << 14)/10)))
+								FB_rate -= FB_RATE_DELTA * 256;
+
 							FB_rate += FB_RATE_DELTA;
 							old_gap = gap;
 						}
@@ -352,13 +356,17 @@ void uac2_device_audio_task(void *pvParameters)
 
 							if (((current_freq.frequency == 88200) && (FB_rate > ((88 << 14) + (7 << 14)/10))) ||
 								((current_freq.frequency == 96000) && (FB_rate > ((96 << 14) + (5 << 14)/10))))
-								FB_rate -= FB_RATE_DELTA * 256;
+								FB_rate -= FB_RATE_DELTA * 128;
 
 							FB_rate -= FB_RATE_DELTA;
 							old_gap = gap;
 						} else
 							if ( (gap > (SPK_BUFFER_SIZE + (SPK_BUFFER_SIZE/2))) && (gap > old_gap)) {
 							LED_Toggle(LED1);
+
+							if ((current_freq.frequency == 88200) && (FB_rate > ((92 << 14) + (2 << 14)/10)))
+								FB_rate -= FB_RATE_DELTA * 256;
+
 							FB_rate += FB_RATE_DELTA;
 							old_gap = gap;
 							}
