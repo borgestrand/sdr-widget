97c9664adf74957368e411c821e2ae9c7a495d68
borgestrand <borge.strand@gmail.com>
2013-06-03 20:49:27 +0200

    Initial rate mismatch

--- src/uac1_usb_specific_request.c
+++ src/uac1_usb_specific_request.c
@@ -588,7 +588,7 @@ void audio_set_cur(void)
 			current_freq.frequency = 44100;
 			// BSB 20130602: code section moved here from uac1_device_audio_task.c
 			FB_rate = (44 << 14) + (1 << 14)/10;
-			FB_rate += 10 * FB_RATE_DELTA; // Debug in order to speed up time to 1st correction
+			FB_rate = (45 << 14) + (1 << 14)/10; // Debug: initial mismatch
 
 			if (FEATURE_BOARD_USBI2S)
 				gpio_clr_gpio_pin(AVR32_PIN_PX16); // BSB 20110301 MUX in 22.5792MHz/2 for AB-1
@@ -601,7 +601,7 @@ void audio_set_cur(void)
 	   		current_freq.frequency = 48000;
 			// BSB 20130602: code section moved here from uac1_device_audio_task.c
 			FB_rate = 48 << 14;
-			FB_rate += 10 * FB_RATE_DELTA; // Debug in order to speed up time to 1st correction
+			FB_rate = 49 << 14; // Debug: Initial mismatch
 
 			if (FEATURE_BOARD_USBI2S)
 				gpio_set_gpio_pin(AVR32_PIN_PX16); // BSB 20110301 MUX in 24.576MHz/2 for AB-1
