516e872d9764501eb0251bd430c52a0e6cbd1160
borgestrand <borge.strand@gmail.com>
2017-04-16 16:58:11 +0200

    Seems functional at very brief test.

--- src/uac1_device_audio_task.c
+++ src/uac1_device_audio_task.c
@@ -540,7 +540,7 @@ void uac1_device_audio_task(void *pvParameters)
 							LED_On(LED0);							// Indicate skipping on module LED
 
 #ifdef USB_STATE_MACHINE_DEBUG
-							print_dbg_char('s');
+							print_dbg_char('s') ;
 #endif
 
 						}
