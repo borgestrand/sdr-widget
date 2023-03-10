a8c29acd27d4e9f09788389aeedd4d306e17c007
Borge Strand-Bergesen <borge.strand@gmail.com>
2023-03-10 14:27:01 +0100

    still doesn't enumerate

--- src/CONFIG/FreeRTOSConfig.h
+++ src/CONFIG/FreeRTOSConfig.h
@@ -179,7 +179,7 @@ to exclude the API function. */
 /* USB device Audio task definitions. */
 #define configTSK_USB_DAUDIO_NAME				((const signed portCHAR *)"USB Device Audio")
 #define configTSK_USB_DAUDIO_STACK_SIZE			256
-#define configTSK_USB_DAUDIO_PRIORITY			(tskIDLE_PRIORITY + 4) // was: +2, no further improvement beyond +4
+#define configTSK_USB_DAUDIO_PRIORITY			(tskIDLE_PRIORITY + 3) // was: +2, no further improvement beyond +4
 #define UAC1_configTSK_USB_DAUDIO_PERIOD		2
 #define UAC2_configTSK_USB_DAUDIO_PERIOD		1
 #define HPSDR_configTSK_USB_DAUDIO_PERIOD		2
