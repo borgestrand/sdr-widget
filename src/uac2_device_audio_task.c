56f8c4b18a88c36c77ed0324ccfe66a89050b9c0
Borge Strand-Bergesen <borge.strand@gmail.com>
2023-12-27 23:45:46 +0100

    si_index_low is reasonably well matched

--- src/uac2_device_audio_task.c
+++ src/uac2_device_audio_task.c
@@ -1270,7 +1270,7 @@ void uac2_device_audio_task(void *pvParameters)
 
 		if (min_last_written_ADC_pos == 0x10101010) {
 			print_dbg_char('d');
-			print_dbg_char_hex(num_samples);
+			print_dbg_char_hex(si_index_low);
 			print_dbg_char('\n');
 			min_last_written_ADC_pos = 0;
 		}
