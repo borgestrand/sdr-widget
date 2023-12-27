75ad2d7d98ae2e2df4932aeeae7ba8e80de7a36e
Borge Strand-Bergesen <borge.strand@gmail.com>
2023-12-27 21:34:48 +0100

    Zero detection is good for a while and then starts reporting silence within non-silence

--- src/Mobo_config.c
+++ src/Mobo_config.c
@@ -1039,7 +1039,7 @@ void mobo_handle_spdif(U32 *si_index_low, S32 *si_score_high, U32 *si_index_high
 			}
 			
 			// Starts out as FALSE, remains TRUE after 1st detection of non-zero audio data 
-			non_silence_det = ( non_silence_det || (abs(sample_temp) > IS_SILENT) || (abs(sample_temp) > IS_SILENT) );
+			non_silence_det = ( non_silence_det || (abs(sample_L) > IS_SILENT) || (abs(sample_R) > IS_SILENT) );
 
 			// It is time consuming to test for each stereo sample!
 			if (we_own_cache) {					// Only write to cache with the right permissions! And only bother with enerby math if it's considered by calling function
