de357d4e2bf1992b33aebe567bfc11313b154ce3
Borge Strand-Bergesen <borge.strand@gmail.com>
2023-12-28 10:49:54 +0100

    Indicating cache write and read - There are cache read operations that make little sense...

--- src/Mobo_config.c
+++ src/Mobo_config.c
@@ -1047,15 +1047,7 @@ void mobo_handle_spdif(U32 *si_index_low, S32 *si_score_high, U32 *si_index_high
 			}
 			
 			// Silence detect v3.0. Starts out as FALSE, remains TRUE after 1st detection of non-zero audio data 
-//			non_silence_det = ( (non_silence_det) || (abs(sample_L) > IS_SILENT) || (abs(sample_R) > IS_SILENT) );
-
-
-			// If we're still silent, keep looking for non-silence
-			if (!non_silence_det) {
-				non_silence_det = ( (abs(sample_L) > IS_SILENT) || (abs(sample_R) > IS_SILENT) );
-			}
-
-
+			non_silence_det = ( (non_silence_det) || (abs(sample_L) > IS_SILENT) || (abs(sample_R) > IS_SILENT) );
 
 			// It is time consuming to test for each stereo sample!
 			if (we_own_cache) {					// Only write to cache and num_samples with the right permissions! And only bother with enerby math if it's considered by calling function
--- src/uac2_device_audio_task.c
+++ src/uac2_device_audio_task.c
@@ -1272,7 +1272,9 @@ void uac2_device_audio_task(void *pvParameters)
 
 		// Start writing from chache to spk_buffer
 		// Don't check input_source again, trust that num_samples > 0 only occurs when cache was legally written to
-		//					gpio_set_gpio_pin(AVR32_PIN_PX31);		// Start copying cache to spk_buffer_X
+
+		gpio_set_gpio_pin(AVR32_PIN_PX31);		// Start copying cache to spk_buffer_X
+
 		num_samples = min(num_samples, SPK_CACHE_MAX_SAMPLES);	// prevent overshoot of cache_L and cache_R
 		if (num_samples > 0) {								// Only start copying when there is something to legally copy
 			i = 0;
@@ -1438,7 +1440,8 @@ void uac2_device_audio_task(void *pvParameters)
 
 		} // end if num_samples > 0
 		// End writing from cache to spk_buffer
-		//					gpio_clr_gpio_pin(AVR32_PIN_PX31);		// End copying DAC data from cache to spk_audio_buffer_X
+		
+		gpio_clr_gpio_pin(AVR32_PIN_PX31);		// End copying DAC data from cache to spk_audio_buffer_X
 
 	} // end while vTask
 }
