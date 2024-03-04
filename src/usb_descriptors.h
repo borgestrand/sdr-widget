5fd66bc76ed7d1ab9b7a2edb3b3a078dab5b5e7c
Borge Strand-Bergesen <borge.strand@gmail.com>
2024-03-04 20:45:32 +0100

    Functional AB-1.2, only tested briefly

--- src/usb_descriptors.h
+++ src/usb_descriptors.h
@@ -481,9 +481,9 @@
   Usb_unicode('0'),\
   Usb_unicode('3'),\
   Usb_unicode('0'),\
-  Usb_unicode('2'),\
+  Usb_unicode('4'),\
   Usb_unicode('0'),\
-  Usb_unicode('0'),\
+  Usb_unicode('1'),\
   Usb_unicode('B'),\
   Usb_unicode('S'),\
   Usb_unicode('B') \
