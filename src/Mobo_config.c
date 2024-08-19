4064f844509c41994f170778189e50d4236a5a1f
Borge Strand-Bergesen <borge.strand@gmail.com>
2024-08-19 12:17:01 +0200

    Moved PX36 to PA5, freeing up PX34 as well

--- src/Mobo_config.c
+++ src/Mobo_config.c
@@ -694,10 +694,6 @@ uint32_t mobo_srd(void) {
 // avr32-gcc -DFEATURE_BOARD_DEFAULT=feature_board_usbi2s -DFEATURE_IMAGE_DEFAULT=feature_image_uac1_audio -DFEATURE_IN_DEFAULT=feature_in_normal -DFEATURE_OUT_DEFAULT=feature_out_normal -DFEATURE_ADC_DEFAULT=feature_adc_none -DFEATURE_DAC_DEFAULT=feature_dac_generic -DFEATURE_LCD_DEFAULT=feature_lcd_hd44780 -DFEATURE_LOG_DEFAULT=feature_log_500ms -DFEATURE_FILTER_DEFAULT=feature_filter_fir -DFEATURE_QUIRK_DEFAULT=feature_quirk_none -DUSB_STATE_MACHINE_DEBUG -DHW_GEN_DIN10 -DFEATURE_PRODUCT_AB1x -DBOARD=SDRwdgtLite -DFREERTOS_USED -I../src/SOFTWARE_FRAMEWORK/DRIVERS/SSC/I2S -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PDCA -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TWIM -I../src/SOFTWARE_FRAMEWORK/UTILS/DEBUG -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/AUDIO -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/CDC -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/portable/GCC/AVR32_UC3 -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/include -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/HID -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB -I../src/CONFIG -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM/DEVICE -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USART -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/WDT -I../src/SOFTWARE_FRAMEWORK/DRIVERS/CPU/CYCLE_COUNTER -I../src/SOFTWARE_FRAMEWORK/DRIVERS/EIC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/RTC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/GPIO -I../src/SOFTWARE_FRAMEWORK/DRIVERS/FLASHC -I../src/SOFTWARE_FRAMEWORK/UTILS/LIBS/NEWLIB_ADDONS/INCLUDE -I../src/SOFTWARE_FRAMEWORK/UTILS/PREPROCESSOR -I../src/SOFTWARE_FRAMEWORK/UTILS -I../src/SOFTWARE_FRAMEWORK/DRIVERS/INTC -I../src/SOFTWARE_FRAMEWORK/BOARDS -I../src -O2 -fdata-sections -Wall -c -fmessage-length=0 -mpart=uc3a3256 -ffunction-sections -masm-addr-pseudos -MMD -c -g -O2 -Wa,-a,-ad srd_test.c > srd_test.lst
 // "/cygdrive/c/Program Files (x86)/Atmel/AVR Tools/AVR Toolchain/bin/avr32-gcc" -DFEATURE_BOARD_DEFAULT=feature_board_usbi2s -DFEATURE_IMAGE_DEFAULT=feature_image_uac1_audio -DFEATURE_IN_DEFAULT=feature_in_normal -DFEATURE_OUT_DEFAULT=feature_out_normal -DFEATURE_ADC_DEFAULT=feature_adc_none -DFEATURE_DAC_DEFAULT=feature_dac_generic -DFEATURE_LCD_DEFAULT=feature_lcd_hd44780 -DFEATURE_LOG_DEFAULT=feature_log_500ms -DFEATURE_FILTER_DEFAULT=feature_filter_fir -DFEATURE_QUIRK_DEFAULT=feature_quirk_none -DUSB_STATE_MACHINE_DEBUG -DHW_GEN_DIN10 -DFEATURE_PRODUCT_AB1x -DBOARD=SDRwdgtLite -DFREERTOS_USED -I../src/SOFTWARE_FRAMEWORK/DRIVERS/SSC/I2S -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PDCA -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TWIM -I../src/SOFTWARE_FRAMEWORK/UTILS/DEBUG -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/AUDIO -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/CDC -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/portable/GCC/AVR32_UC3 -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/include -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/HID -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB -I../src/CONFIG -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM/DEVICE -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USART -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/WDT -I../src/SOFTWARE_FRAMEWORK/DRIVERS/CPU/CYCLE_COUNTER -I../src/SOFTWARE_FRAMEWORK/DRIVERS/EIC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/RTC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/GPIO -I../src/SOFTWARE_FRAMEWORK/DRIVERS/FLASHC -I../src/SOFTWARE_FRAMEWORK/UTILS/LIBS/NEWLIB_ADDONS/INCLUDE -I../src/SOFTWARE_FRAMEWORK/UTILS/PREPROCESSOR -I../src/SOFTWARE_FRAMEWORK/UTILS -I../src/SOFTWARE_FRAMEWORK/DRIVERS/INTC -I../src/SOFTWARE_FRAMEWORK/BOARDS -I../src -O2 -fdata-sections -Wall -c -fmessage-length=0 -mpart=uc3a3256 -ffunction-sections -masm-addr-pseudos -MMD -c -g -O2 -Wa,-a,-ad srd_test.c > srd_test.lst
 //
-// Alternatively:
-// avr32-gcc -DFEATURE_BOARD_DEFAULT=feature_board_usbi2s -DFEATURE_IMAGE_DEFAULT=feature_image_uac1_audio -DFEATURE_IN_DEFAULT=feature_in_normal -DFEATURE_OUT_DEFAULT=feature_out_normal -DFEATURE_ADC_DEFAULT=feature_adc_none -DFEATURE_DAC_DEFAULT=feature_dac_generic -DFEATURE_LCD_DEFAULT=feature_lcd_hd44780 -DFEATURE_LOG_DEFAULT=feature_log_500ms -DFEATURE_FILTER_DEFAULT=feature_filter_fir -DFEATURE_QUIRK_DEFAULT=feature_quirk_none -DUSB_STATE_MACHINE_DEBUG -DHW_GEN_DIN10 -DFEATURE_PRODUCT_AB1x -DBOARD=SDRwdgtLite -DFREERTOS_USED -I../src/SOFTWARE_FRAMEWORK/DRIVERS/SSC/I2S -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PDCA -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TWIM -I../src/SOFTWARE_FRAMEWORK/UTILS/DEBUG -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/AUDIO -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/CDC -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/portable/GCC/AVR32_UC3 -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/include -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/HID -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB -I../src/CONFIG -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM/DEVICE -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USART -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/WDT -I../src/SOFTWARE_FRAMEWORK/DRIVERS/CPU/CYCLE_COUNTER -I../src/SOFTWARE_FRAMEWORK/DRIVERS/EIC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/RTC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/GPIO -I../src/SOFTWARE_FRAMEWORK/DRIVERS/FLASHC -I../src/SOFTWARE_FRAMEWORK/UTILS/LIBS/NEWLIB_ADDONS/INCLUDE -I../src/SOFTWARE_FRAMEWORK/UTILS/PREPROCESSOR -I../src/SOFTWARE_FRAMEWORK/UTILS -I../src/SOFTWARE_FRAMEWORK/DRIVERS/INTC -I../src/SOFTWARE_FRAMEWORK/BOARDS -I../src -O2 -fdata-sections -Wall -c -fmessage-length=0 -mpart=uc3a3256 -ffunction-sections -masm-addr-pseudos -MMD -S -fverbose-asm -g -O2 srd_test.c
-// avr32-gcc -DFEATURE_BOARD_DEFAULT=feature_board_usbi2s -DFEATURE_IMAGE_DEFAULT=feature_image_uac1_audio -DFEATURE_IN_DEFAULT=feature_in_normal -DFEATURE_OUT_DEFAULT=feature_out_normal -DFEATURE_ADC_DEFAULT=feature_adc_none -DFEATURE_DAC_DEFAULT=feature_dac_generic -DFEATURE_LCD_DEFAULT=feature_lcd_hd44780 -DFEATURE_LOG_DEFAULT=feature_log_500ms -DFEATURE_FILTER_DEFAULT=feature_filter_fir -DFEATURE_QUIRK_DEFAULT=feature_quirk_none -DUSB_STATE_MACHINE_DEBUG -DHW_GEN_DIN10 -DFEATURE_PRODUCT_AB1x -DBOARD=SDRwdgtLite -DFREERTOS_USED -I../src/SOFTWARE_FRAMEWORK/DRIVERS/SSC/I2S -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PDCA -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TWIM -I../src/SOFTWARE_FRAMEWORK/UTILS/DEBUG -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/AUDIO -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/CDC -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/portable/GCC/AVR32_UC3 -I../src/SOFTWARE_FRAMEWORK/SERVICES/FREERTOS/Source/include -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB/CLASS/HID -I../src/SOFTWARE_FRAMEWORK/SERVICES/USB -I../src/CONFIG -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM/DEVICE -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB/ENUM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USBB -I../src/SOFTWARE_FRAMEWORK/DRIVERS/USART -I../src/SOFTWARE_FRAMEWORK/DRIVERS/TC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/WDT -I../src/SOFTWARE_FRAMEWORK/DRIVERS/CPU/CYCLE_COUNTER -I../src/SOFTWARE_FRAMEWORK/DRIVERS/EIC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/RTC -I../src/SOFTWARE_FRAMEWORK/DRIVERS/PM -I../src/SOFTWARE_FRAMEWORK/DRIVERS/GPIO -I../src/SOFTWARE_FRAMEWORK/DRIVERS/FLASHC -I../src/SOFTWARE_FRAMEWORK/UTILS/LIBS/NEWLIB_ADDONS/INCLUDE -I../src/SOFTWARE_FRAMEWORK/UTILS/PREPROCESSOR -I../src/SOFTWARE_FRAMEWORK/UTILS -I../src/SOFTWARE_FRAMEWORK/DRIVERS/INTC -I../src/SOFTWARE_FRAMEWORK/BOARDS -I../src -O2 -fdata-sections -Wall -c -fmessage-length=0 -mpart=uc3a3256 -ffunction-sections -masm-addr-pseudos -MMD -S -g -O2 srd_test.c
-//
 // A good asm syntax list:
 // http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html
 //
@@ -723,29 +719,30 @@ uint32_t mobo_srd_asm2(void) {
 
 	// see srd_test03.c and srd_test03.lst
 
-	// HW_GEN_SPRX: Moved from PX09, pin 49 to PA05, pin 124, to PX36, pin 44 same as used by other code
+	// HW_GEN_SPRX: Moved to PA05
 
 	// Recompile prototype c to change io pin!
 	// Test is done for up to 1 half period, then 2 full periods
 
-	gpio_enable_gpio_pin(AVR32_PIN_PX36);	// Enable GPIO pin, not special IO (also for input). Needed?
+	gpio_enable_gpio_pin(AVR32_PIN_PA05);	// Enable GPIO pin, not special IO (also for input). Needed?
 	
-	// PA05 is GPIO. PX26 and PX36 are special purpose clock pins
+	// PA05 is GPIO. PX26 and PA05 are special purpose clock pins
 
 	asm volatile(
+	
 		//		"ssrf	16				\n\t"	// Disable global interrupt
 		"mov	%0, 	2000	\n\t"	// Load timeout
-		"mov	r9,		-60928	\n\t"	// Immediate load, set up pointer to PX36, recompile C for other IO pin, do once
+		"mov	r9,		-61440	\n\t"	// Immediate load, set up pointer to PA05, (0xFFFF1000) recompile C for other IO pin, do once
 
 		// If bit is 0, branch to loop while 0. If bit was 1, continue to loop while 1
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"brne	S3				\n\t"	// Branch if %0 bit 11 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 
 		// Wait while bit is 1, then count two half periods
-		"S0:					\n\t"	// Loop while PX36 is 1
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S0:					\n\t"	// Loop while PA05 is 1
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"brne	S0_done			\n\t"	// Branch if %0 bit 11 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S0				\n\t"	// Not done counting down
@@ -754,18 +751,18 @@ uint32_t mobo_srd_asm2(void) {
 
 		"mfsr	r10, 264		\n\t"	// Load 1st cycle counter into r10
 
-		"S1:					\n\t"	// Loop while PX36 is 0
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S1:					\n\t"	// Loop while PA05 is 0
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"breq	S1_done			\n\t"	// Branch if %0 bit 4 was 1 (bit was 1, Z becomes 1 i.e. equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S1				\n\t"	// Not done counting down
 		"rjmp	SCOUNTD			\n\t"	// Countdown reached
 		"S1_done:				\n\t"
 
-		"S2:					\n\t"	// Loop while PX36 is 1
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S2:					\n\t"	// Loop while PA05 is 1
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"brne	S2_done			\n\t"	// Branch if %0 bit 4 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S2				\n\t"	// Not done counting down
@@ -776,9 +773,9 @@ uint32_t mobo_srd_asm2(void) {
 
 
 		// Wait while bit is 0, then count two half periods
-		"S3:					\n\t"	// Loop while PX36 is 0
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S3:					\n\t"	// Loop while PA05 is 0
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"breq	S3_done			\n\t"	// Branch if %0 bit 4 was 1 (bit was 1, Z becomes 1 i.e. equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S3				\n\t"	// Not done counting down
@@ -787,18 +784,18 @@ uint32_t mobo_srd_asm2(void) {
 
 		"mfsr	r10, 264		\n\t"	// Load 1st cycle counter into r10
 
-		"S4:					\n\t"	// Loop while PX36 is 1
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S4:					\n\t"	// Loop while PA05 is 1
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"brne	S4_done			\n\t"	// Branch if %0 bit 4 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S4				\n\t"	// Not done counting down
 		"rjmp	SCOUNTD			\n\t"	// Countdown reached
 		"S4_done:				\n\t"
 
-		"S5:					\n\t"	// Loop while PX36 is 0
-		"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-		"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+		"S5:					\n\t"	// Loop while PA05 is 0
+		"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+		"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 		"breq	S5_done			\n\t"	// Branch if %0 bit 4 was 1 (bit was 1, Z becomes 1 i.e. equal)
 		"sub	%0,	1			\n\t"	// Count down
 		"brne	S5				\n\t"	// Not done counting down
@@ -885,24 +882,22 @@ uint32_t mobo_wait_LRCK_RX_asm(void) {
 
 	// see srd_test03.c and srd_test03.lst
 
-	// HW_GEN_SPRX: Moved from PX09, pin 49 to PA05, pin 124, to PX36, pin 44 same as used by other code
+	// Module carrier: moved to PA05
 
 	// Recompile prototype c to change io pin!
 	// Test is done for up to 1 half period, then 2 full periods
 
-	gpio_enable_gpio_pin(AVR32_PIN_PX36);	// Enable GPIO pin, not special IO (also for input). Needed?
+	gpio_enable_gpio_pin(AVR32_PIN_PA05);	// Enable GPIO pin, not special IO (also for input). Needed?
 	
-	// PA05 is GPIO. PX26 and PX36 are special purpose clock pins
-
 	asm volatile(
 	//		"ssrf	16				\n\t"	// Disable global interrupt
 	"mov	%0, 	500		\n\t"	// Load timeout
-	"mov	r9,		-60928	\n\t"	// Immediate load, set up pointer to PX36, recompile C for other IO pin, do once
+	"mov	r9,		-61440	\n\t"	// Immediate load, set up pointer to PA05, recompile C for other IO pin, do once
 
 /*
-	"S0x:					\n\t"	// Loop while PX36 is 1
-	"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-	"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+	"S0x:					\n\t"	// Loop while PA05 is 1
+	"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+	"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 	"brne	S0x_done		\n\t"	// Branch if %0 bit 11 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 	"sub	%0,	1			\n\t"	// Count down
 	"brne	S0x				\n\t"	// Not done counting down
@@ -910,27 +905,27 @@ uint32_t mobo_wait_LRCK_RX_asm(void) {
 	"S0x_done:				\n\t"
 */
 
-	"S1x:					\n\t"	// Loop while PX36 is 0
-	"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-	"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+	"S1x:					\n\t"	// Loop while PA05 is 0
+	"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+	"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 	"breq	S1x_done		\n\t"	// Branch if %0 bit 4 was 1 (bit was 1, Z becomes 1 i.e. equal)
 	"sub	%0,	1			\n\t"	// Count down
 	"brne	S1x				\n\t"	// Not done counting down
 	"rjmp	SCOUNTD_RX		\n\t"	// Countdown reached
 	"S1x_done:				\n\t"
 
-	"S2x:					\n\t"	// Loop while PX36 is 1
-	"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-	"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+	"S2x:					\n\t"	// Loop while PA05 is 1
+	"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+	"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 	"brne	S2x_done		\n\t"	// Branch if %0 bit 4 was 0 (bit was 0, Z becomes 0 i.e. not equal)
 	"sub	%0,	1			\n\t"	// Count down
 	"brne	S2x				\n\t"	// Not done counting down
 	"rjmp	SCOUNTD_RX		\n\t"	// Countdown reached
 	"S2x_done:				\n\t"
 
-	"S3x:					\n\t"	// Loop while PX36 is 0
-	"ld.w	r8, 	r9[96]	\n\t"	// Load PX36 (and surroundings?) into r8, 		recompile C for other IO pin
-	"bld	r8, 	23		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
+	"S3x:					\n\t"	// Loop while PA05 is 0
+	"ld.w	r8, 	r9[96]	\n\t"	// Load PA05 (and surroundings?) into r8, 		recompile C for other IO pin
+	"bld	r8, 	5		\n\t"	// Bit load to Z and C, similar to above line,	recompile c for other IO pin
 	"breq	S3x_done		\n\t"	// Branch if %0 bit 4 was 1 (bit was 1, Z becomes 1 i.e. equal)
 	"sub	%0,	1			\n\t"	// Count down
 	"brne	S3x				\n\t"	// Not done counting down
@@ -956,14 +951,10 @@ uint32_t mobo_wait_LRCK_TX_asm(void) {
 
 	// see srd_test03.c and srd_test03.lst
 
-	// HW_GEN_SPRX: Moved from PX09, pin 49 to PA05, pin 124, to PX36, pin 44 same as used by other code
-
 	// Recompile prototype c to change io pin!
 	// Test is done for up to 1 half period, then 2 full periods
 
-//	gpio_enable_gpio_pin(AVR32_PIN_PX27);	// DON'T! Enable GPIO pin, not special IO (also for input). Needed?
-	
-	// PA05 is GPIO. PX26 and PX36 are special purpose clock pins
+	//	gpio_enable_gpio_pin(AVR32_PIN_PX27);	// DON'T! Enable GPIO pin, not special IO (also for input). Needed?
 
 	asm volatile(
 	//		"ssrf	16				\n\t"	// Disable global interrupt
