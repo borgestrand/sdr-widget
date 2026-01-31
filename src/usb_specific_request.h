3caaf2e302c12f10aac8b460ce5a9fbc48753266
Borge Strand-Bergesen <borge.strand@gmail.com>
2026-01-31 15:05:44 +0100

    Removed some excess gap definitions

--- src/usb_specific_request.h
+++ src/usb_specific_request.h
@@ -118,16 +118,6 @@ extern volatile  Bool  usb_alternate_setting_changed, usb_alternate_setting_out_
 #define SPK_GAP_LSKIP DAC_BUFFER_UNI * 1 / 8	// Almost a full buffer down in distance => enable skip/insert
 #define SPK_GAP_SIOFS DAC_BUFFER_UNI * 2 / 8	// How far to move back or ahead based on known information on source
 
-// SPDIF buffer skip/insert limits
-#define SPK_GAP_L3	DAC_BUFFER_UNI * 2.0 / 8	// Lower limit for safe operation, starting point for skip/insert
-#define SPK_GAP_LD	DAC_BUFFER_UNI * 2.1 / 8	// Mega skip/insert landing position
-#define SPK_GAP_LM	DAC_BUFFER_UNI * 2.4 / 8	// Narrowest starting point for mega skip/insert
-#define SPK_GAP_LX	DAC_BUFFER_UNI * 1.8 / 8	// Starting point for oscillator toggle
-
-#define SPK_GAP_U3	DAC_BUFFER_UNI * 6.0 / 8	// Upper limit for safe operation, starting point for skip/insert
-#define SPK_GAP_UD	DAC_BUFFER_UNI * 5.9 / 8	// Mega skip/insert landing position
-#define SPK_GAP_UM	DAC_BUFFER_UNI * 5.6 / 8	// Narrowest starting point for mega skip/insert
-#define SPK_GAP_UX	DAC_BUFFER_UNI * 6.2 / 8	// Starting point for oscillator toggle
 
 // Various other defines
 #define	SPK_PACKETS_PER_GAP_CALCULATION 8		// This is UAC1 which counts in ms. Gap calculation every 8ms, EP reporting every 32
